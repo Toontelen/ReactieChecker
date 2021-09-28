55b398cbae375f808254ce0a4baca2294ed3de50
toon.telen@gmail.com <toon.telen@gmail.com>
2021-09-28 10:48:31 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,12 +6,16 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.check_lijst()
 
     # Any code you write here will run when the form opens.
     
 
   def text_area_1_change(self, **event_args):
     """This method is called when the text in this text area is edited"""
+    tekst=self.text_area_1.text
+    
+  def check_lijst(self):
     tekst=self.text_area_1.text
     leidingslijst=list(['Toon Telen', 'Ken Vanoosterhout','Stan Schepers'])
     for leiding in leidingslijst:
