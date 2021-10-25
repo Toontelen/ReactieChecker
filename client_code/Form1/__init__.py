4dce851a1a18f6fa0691873941f44f2521f48ed3
toon.telen@gmail.com <toon.telen@gmail.com>
2021-10-25 08:17:28 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,14 +6,27 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.check_lijst()
+    
 
     # Any code you write here will run when the form opens.
     
 
   def text_area_1_change(self, **event_args):
     """This method is called when the text in this text area is edited"""
+    text=self.ReactiesInput
+    print(text)
+    
+    def get_input(self):
+      return text
+    
  
         
     
+
+  def ButtonCheckLeiding_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    text= self.text_area_1_change.get_input()
+    
+    pass
+
 
