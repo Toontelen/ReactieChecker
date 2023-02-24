4397aeec9b9429edc85a33de23759888383ccf02
toon.telen@gmail.com <toon.telen@gmail.com>
2023-02-24 14:08:33 +0000

    Edited Facebookfilter

--- client_code/Facebookfilter/__init__.py
+++ client_code/Facebookfilter/__init__.py
@@ -101,6 +101,11 @@ class Facebookfilter(FacebookfilterTemplate):
 
   def file_loader_1_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
     
 
 
