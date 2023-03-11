8379fd8c8abf2d784d1f74614bc9123ba899132f
toon.telen@gmail.com <toon.telen@gmail.com>
2023-03-11 08:21:22 +0000

    Edited Facebookfilter

--- client_code/Facebookfilter/__init__.py
+++ client_code/Facebookfilter/__init__.py
@@ -5,6 +5,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 from teller import teller
+import re
+# import pandas as pd
 # from Facebookfilter import Facebookfilter
 
 class Facebookfilter(FacebookfilterTemplate):
@@ -106,6 +108,104 @@ class Facebookfilter(FacebookfilterTemplate):
     """This method is called when the link is clicked"""
     self.content_panel.clear()
     self.content_panel.add_component(teller())
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    leidingslijst=list(["Kobe Lobbinger",
+"Sien Telen",
+"Anna Vandercappellen",
+"Stan Schepers",
+"Frederik Vandael",
+"Merle Daniels",
+"Jonas Snellings",
+"Toon Telen",
+"Jeroen Steegmans",
+"Ken Vanoosterhout",
+"Liam Reekmans",
+"Kay Vanbaelen",
+"Briek Moons",
+"Thibauld Buteneers",
+"Lander Carmans",
+"Fien Bokken",
+"Flore Vandercappellen",
+"Daan Vanreppelen",
+"Sara Franssens",
+"Ranya Kitsinis",
+"Lorien Zeelmaekers",
+"Nikola Juszczak",
+"Ellen Franssens",
+"Nore Daniels",
+"Jan Vanrijckeghem",
+"Britt Kumbruck",
+"Lara Kumbruck",
+"Senne Bammens",
+"Ella Schepers",
+"An-Sofie Vandael",
+"Siebe Lodewijckx",
+"Ian Vanoosterhout",
+"Cel Sterkens",
+"Flore Nys",
+"Lies Bokken",
+"Lola Moons",
+"Tibo Leyssens",
+"Nele Ganne",
+"Arno Vanrijckeghem",
+"Otto Vossen",
+"Pieter Carmans",
+"Matteo Michetti",
+"Sam Gielen", 
+"Ferre Kumbruck",
+"Thibo Minten", 
+"Mano De Raeve",
+"Axel Tielens",
+"Robin Burnham",
+"Liam Beckx",
+"Anna Kuppers"])
+    leidingslijst2=list() 
+    for leider in leidingslijst:
+      leider=leider.strip()
+      leider=leider.lower()
+      leidingslijst2.append(leider)
+    reacties= self.text_area_1.text
+    score_dict=dict()
+    tekst=[]
+    for index, zin in enumerate(reacties):
+      zin=zin.lower()
+      zin=zin.strip()
+      tekst.append(zin)
+
+    for i, t in enumerate(tekst):
+    #     print(i,t)
+        match = re.search('aanwezig', t)
+        if match:
+            if tekst[i-1] in leidingslijst2:
+    #             print("leiding gevonden:", tekst[i-1])
+                score_dict[tekst[i-1]]=+1  
+            elif tekst[i-2] in leidingslijst2:
+    #             print("leiding gevonden:", tekst[i-2])
+                score_dict[tekst[i-2]]=+1 
+            elif tekst[i-3] in leidingslijst2:
+    #             print("leiding gevonden:", tekst[i-3])
+                score_dict[tekst[i-3]]=+1 
+            
+    for leiding in leidingslijst2:
+        if leiding not in score_dict.keys():
+            print(leiding, "niet gevonden!")
+            score_dict[leiding]=0
+        
+    sortedDict = dict(sorted(score_dict.items(), key=lambda x: x[0].lower()) )
+        
+    for k,v in sortedDict.items():
+      print('{}:{}'.format(k,v))
+        
+    aantal_aanwezig=len(sortedDict.items())
+    print("aantal leiding aanwezig:",aantal_aanwezig)
+    
+    # print(sortedDict.keys())
+    self.output.text=sortedDict.items()
+    self.text_box_1.text= (f"Er is {len(sortedDict.keys())} leiding afwezig")
+
+
     
 
     
